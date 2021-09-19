b8d307f46a985c5cf346ea4efe46c43540edc423
kalinh <taikhoanxzc004@gmail.com>
2021-09-19 11:24:55 +0800

    Create nkninstall.sh

---
+++ nkninstall.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+clear
+echo "============================================================================================="
+echo "                              WELCOME TO NKNAgent FAST DEPLOY!"
+echo "============================================================================================="
+echo
+echo "This script will automatically provision a node as you configured it in your snippet."
+echo "So grab a coffee, lean back or do something else - installation will take about 5 minutes."
+echo -e "============================================================================================="
+echo
+echo "Hardening your OS..."
+echo "---------------------------"
+export DEBIAN_FRONTEND=noninteractive
+apt-get -qq update
+apt-get -qq upgrade -y
+echo "Installing necessary libraries..."
+echo "---------------------------"
+apt-get install make curl git unzip whois makepasswd -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
+apt-get install unzip jq -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
+#curl --insecure --data "secret=7482da1b67fd4d21ffb6c3ca847e99a91c269181" https://api.nknag.org/fast-deploy/callbacks/created
+sudo groupadd nknag
+sudo useradd -g nknag -b /home -s /usr/sbin/nologin nknag
+mkdir -p /home/nkn/.ssh
+mkdir -p /home/nkn/.nknag
+adduser nknag sudo
+chsh -s /bin/bash nknag
+PASSWORD=$(mkpasswd zktcqx0L)
+usermod --password $PASSWORD nknag > /dev/null 2>&1
+cd /home/nkn
+echo "Installing NKN Commercial..."
+echo "---------------------------"
+wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1
+unzip -qq linux-amd64.zip
+cd linux-amd64
+cat >config.json <<EOF
+{
+    "nkn-node": {
+       "args": "--no-nat",
+       "noRemotePortCheck": true
+    }
+}
+EOF
+#./nkn-commercial -b nknagXXXXGKct2cZuhSGW6xqiqeFVd5nJtAzg -c /home/nkn/linux-amd64/config.json -d /home/nkn/nkn-commercial -u nknag install > /dev/null 2>&1
+./nkn-commercial -b NKNNdBrKQq2idX6cACTJVRCoPRfdSb93sMht -c /home/nkn/linux-amd64/config.json -d /home/nkn/nkn-commercial -u nknag install > /dev/null 2>&1
+chown -R nknag:nknag /home/nkn
+chmod -R 755 /home/nkn
+echo "Waiting for wallet generation..."
+echo "---------------------------"
+while [ ! -f /home/nkn/nkn-commercial/services/nkn-node/wallet.json ]; do sleep 10; done
+echo "Downloading pruned snapshot..."
+echo "---------------------------"
+#curl --insecure --data "secret=7482da1b67fd4d21ffb6c3ca847e99a91c269181" https://api.nknag.org/fast-deploy/callbacks/downloading-snapshot
+cd /home/nkn/nkn-commercial/services/nkn-node/
+systemctl stop nkn-commercial.service
+rm -rf ChainDB
+#wget -c https://nkn.org/ChainDB_pruned_latest.tar.gz -O - | tar -xz
+#wget -c "https://onedrive.live.com/download?cid=B409482F4F7DA231&resid=B409482F4F7DA231%21106&authkey=AEvytR8r9nemPqw" -O - | tar -xz
+sudo -u nknag curl -L -s "https://nkn.org/ChainDB_pruned_latest.tar.gz" | sudo -u nknag tar -zxf -
+#curl --insecure --data "secret=7482da1b67fd4d21ffb6c3ca847e99a91c269181" https://api.nknag.org/fast-deploy/callbacks/unzipping-snapshot
+chown -R nknag:nknag ChainDB/
+systemctl start nkn-commercial.service
+echo "Applying finishing touches..."
+echo "---------------------------"
+addr=$(jq -r .Address /home/nkn/nkn-commercial/services/nkn-node/wallet.json)
+cd /home/nkn/.nknag
+cat >donationcheck <<EOF
+cd /home/nkn/linux-amd64
+#response=\$(curl --write-out %{http_code} --silent --output /dev/null "https://openapi.nkn.org/api/v1/addresses/ZZZYYY/hasMinedToAddress/nknagXXXXGKct2cZuhSGW6xqiqeFVd5nJtAzg")
+response=\$(curl --write-out %{http_code} --silent --output /dev/null "https://openapi.nkn.org/api/v1/addresses/ZZZYYY/hasMinedToAddress/NKNNdBrKQq2idX6cACTJVRCoPRfdSb93sMht")
+if [ "\$response" -eq 202 ]
+then
+systemctl stop nkn-commercial.service
+./nkn-commercial -b NKNNdBrKQq2idX6cACTJVRCoPRfdSb93sMht -c /home/nkn/linux-amd64/config.json -d /home/nkn/nkn-commercial -u nknag install
+chown -R nknag:nknag /home/nkn
+chmod -R 755 /home/nkn
+cd /home/nkn/.nknag
+crontab -l > tempcron
+sed -i '$ d' tempcron
+crontab tempcron > /dev/null 2>&1
+rm tempcron > /dev/null 2>&1
+#curl --insecure --data "secret=7482da1b67fd4d21ffb6c3ca847e99a91c269181" https://api.nknag.org/fast-deploy/callbacks/donated
+rm /home/nkn/.nknag/donationcheck > /dev/null 2>&1
+fi
+EOF
+sed -i "s/ZZZYYY/$addr/g" donationcheck
+crontab -l > tempcron
+echo "43 * * * * /home/nkn/.nknag/donationcheck >/dev/null 2>&1" >> tempcron
+crontab tempcron
+rm tempcron
+chown -R nknag:nknag /home/nkn
+chmod -R 755 /home/nkn
+#curl --insecure --data "secret=7482da1b67fd4d21ffb6c3ca847e99a91c269181" https://api.nknag.org/fast-deploy/callbacks/finish-install
+sleep 2
+clear
+echo
+echo
+echo
+echo
+echo "                                  --------------------------"
+echo "                                  |  NKNAgent FAST-DEPLOY  |"
+echo "                                  --------------------------"
+echo
+echo "============================================================================================="
+echo "   NKN ADDRESS OF THIS NODE: $addr"
+echo "   PASSWORD FOR THIS WALLET IS: zktcqx0L"
+echo "============================================================================================="
+echo "   ALL MINED NKN WILL GO TO: NKNNdBrKQq2idX6cACTJVRCoPRfdSb93sMht"
+echo "   (FIRST MINING WILL BE DONATED TO NKNAgent-TEAM)"
+echo "============================================================================================="
+echo
+echo "You can now disconnect from your terminal. The node will automatically appear in nknag after 1 minute."
+echo
+echo
+echo
+echo
